e9b714c0e21b31140e36ceb05e29ba5df12dcbea
Lukas Fittl <lukas@fittl.com>
2016-08-15 23:45:18 -0700

    Add separate kernelVersion field to host information.
    This is mostly intended for Linux, where we are returning the OS version
    in the PlatformVersion field, which seems reasonable. Often it is still
    useful to know which Linux kernel is running (uname -r).
    
    For FreeBSD and Darwin the kernel version matches the platform version,
    since they previously used the kernel version for the platform version.
    
    For Windows the kernel version is empty, since there is no clear way
    to determine it.

--- host/host_linux.go
+++ host/host_linux.go
@@ -44,6 +44,10 @@ func Info() (*InfoStat, error) {
 		ret.PlatformFamily = family
 		ret.PlatformVersion = version
 	}
+	kernelVersion, err := KernelVersion()
+	if err == nil {
+		ret.KernelVersion = kernelVersion
+	}
 
 	system, role, err := Virtualization()
 	if err == nil {
@@ -352,6 +356,20 @@ func PlatformInformation() (platform string, family string, version string, err 
 
 	return platform, family, version, nil
 
+}
+
+func KernelVersion() (version string, err error) {
+	uname, err := exec.LookPath("uname")
+	if err != nil {
+		return "", err
+	}
+
+	out, err := invoke.Command(uname, "-r")
+	if err == nil {
+		version = strings.ToLower(strings.TrimSpace(string(out)))
+	}
+
+	return version, nil
 }
 
 func getRedhatishVersion(contents []string) string {
